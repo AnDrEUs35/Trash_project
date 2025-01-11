93f6a9da9c0ceedcf9058239ff224e38a6ba16d6
moaxgetsu <65217332+Nmxxxxx@users.noreply.github.com>
2025-01-11 12:52:32 +0000

    asdasd

--- src/handler.py
+++
@@ -1,7 +0,0 @@
-
-class Handler:
-    def __init__(self, result_data, output_path):
-        pass
-
-    def get_graphic_output(self, user_graphical_data):
-        pass
--- src/solver.py
+++
@@ -1,6 +0,0 @@
-class Solver:
-    def __init__(self, init_data):
-        pass
-
-    def run_service(self):
-        pass
