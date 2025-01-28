2c0c1b52b1d46059bd78b92e64e196113e7e05ae
moaxgetsu <65217332+Nmxxxxx@users.noreply.github.com>
2025-01-28 15:47:30 +0000

    jhhghliuhg

--- src/parser.py
+++ src/parser.py
@@ -1,17 +1,33 @@
 import requests
 import json
-import h5py
 from sgp4.api import Satrec
 from astropy.time import Time
 import sys
 from banner import run
 
-run()
+class Satellite:
+    def __init__(self, name, line1, line2):
+        self.name = name
+        self.line1 = line1
+        self.line2 = line2
+        self.coords = None
+        self.velocity = None
 
-class GetData:
-    # Функция для получения TLE данных
+    def calculate_pos(self):
+        satrec = Satrec.twoline2rv(self.line1, self.line2)
+        t = Time.now() 
+        error_code, teme_p, teme_v = satrec.sgp4(t.jd1, t.jd2)  # в км и км/с
+        if error_code == 0:
+            self.coords = list(teme_p)
+            self.velocity = list(teme_v)
+        else:
+            print(f"Ошибка при вычислении {self.name}: код ошибки {error_code}")
+
+class TLEFetch:
+    @staticmethod
     def get_tle_data(url):
         response = requests.get(url)
+
         if response.status_code == 200:
             tle_data = response.text.splitlines()
             satellites = []
@@ -22,11 +38,11 @@ class GetData:
 
             for i in range(0, len(tle_data) - 2, 3):
                 if i + 2 < len(tle_data):
-                    satellite = {
-                        "name": tle_data[i].strip(),
-                        "line1": tle_data[i + 1].strip(),
-                        "line2": tle_data[i + 2].strip()
-                    }
+                    satellite = Satellite(
+                        name=tle_data[i].strip(),
+                        line1=tle_data[i + 1].strip(),
+                        line2=tle_data[i + 2].strip()
+                    )
                     satellites.append(satellite)
                 else:
                     print(f"Пропущена строка {i}, Причина: недостаток данных")
@@ -35,70 +51,56 @@ class GetData:
             print("Ошибка при получении данных")
             sys.exit()
 
-# Спутники
-active_url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
-active_satellites = GetData.get_tle_data(active_url)
+class SatelliteProcess:
+    def __init__(self):
+        self.data = {
+            "satellites": [],
+            "trash": [],
+            "user_properties": {
+                "time": 20,
+                "time_step": 4,
+            }
+        }
 
-# Мусор
-debris_url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=fengyun-1c-debris&FORMAT=tle"
-debris_satellites = GetData.get_tle_data(debris_url)
+    def process_satellite(self, satellites):
+        for satellite in satellites:
+            satellite.calculate_pos()
+            self.data["satellites"].append({
+                "name": satellite.name,
+                "coords": satellite.coords,  
+                "velocity": satellite.velocity  
+            })
 
-# Проверка полученных данных о мусоре
-if not debris_satellites:
-    print("Нет данных о мусоре.")
-else:
-    print(f"Получено {len(debris_satellites)} спутников мусора.")
+    def process_trash(self, trash):
+        for index, satellite in enumerate(trash, start=1):
+            satellite.calculate_pos()
+            self.data["trash"].append({
+                "index": index,
+                "coords": satellite.coords, 
+                "velocity": satellite.velocity 
+            })
 
-# Структура для хранения данных
-data = {
-    "satellites": [],
-    "trash": [],
-    "user_properties": {
-        "time": 20,
-        "time_step": 4,
-    }
-}
+    def save(self, file_name='data.json'):
+        with open(file_name, 'w') as json_file:
+            json.dump(self.data, json_file, indent=4)
+        print("Все данные успешно сохранены")
 
-count = 0
+def main():
+    # Спутники
+    active_url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
+    active_satellites = TLEFetch.get_tle_data(active_url)
 
-# Обработка активных спутников
-for satellite in active_satellites:
-    line1 = satellite['line1']
-    line2 = satellite['line2']
-    
-    satrec = Satrec.twoline2rv(line1, line2)
+    # Мусор
+    debris_url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=fengyun-1c-debris&FORMAT=tle"
+    debris_satellites = TLEFetch.get_tle_data(debris_url)
 
-    t = Time.now() 
-    error_code, teme_p, teme_v = satrec.sgp4(t.jd1, t.jd2)  # в км и км/с
+    processor = SatelliteProcess()
+    processor.process_satellite(active_satellites)
+    processor.process_trash(debris_satellites)
+    processor.save()
 
-    data["satellites"].append({
-        "name": satellite['name'],
-        "coords": list(teme_p),  # Преобразуем в список
-        "velocity": list(teme_v)  # Преобразуем в список
-    })
+    print("Все данные были успешно записаны")
 
-    count += 1
-
-# Обработка данных о мусоре
-for index, satellite in enumerate(debris_satellites, start=1):
-    line1 = satellite['line1']
-    line2 = satellite['line2']
-    
-    satrec = Satrec.twoline2rv(line1, line2)
-
-    t = Time.now() 
-    error_code, teme_p, teme_v = satrec.sgp4(t.jd1, t.jd2)  # в км и км/с
-
-    data["trash"].append({
-        "index": index,
-        "coords": list(teme_p),  # Преобразуем в список
-        "velocity": list(teme_v)  # Преобразуем в список
-    })
-
-# Сохранение данных в JSON файл
-with open('data.json', 'w') as json_file:
-    json.dump(data, json_file, indent=4)
-
-print("Все данные успешно сохранены")
-
-print(count)
+if __name__ == "__main__":
+    run()
+    main()
