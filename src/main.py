4049f1082c38d1b39354c78bd2dd03a6efedbbd5
moaxgetsu <65217332+Nmxxxxx@users.noreply.github.com>
2025-02-01 11:47:11 +0000

    sad

--- src/main.py
+++ src/main.py
@@ -2,14 +2,11 @@ from parser import Parser
 
 
 def run(data, output_path):
-    # parser = Parser(3)
-    # init_data = parser.get_hdf5_file()
-    # solver = Solver(init_data=init_data)
-    # result_data = solver.run_service()
-
-    # handler = Handler(result_data=result_data, output_path=output_path)
-    # handler.get_graphic_output(parser.get_graphic_data())
+    parser = Parser(data, output_path)
+    parser.process()
+    parser.save_solv_data()
+    print("Все данные были успешно сохранены")
 
 
-# if __name__ == "__main__":
-#     run(data= "./test/data.json", output_path="./test/result_solv_data")
+if __name__ == "__main__":
+    run(data= "./test/data.json", output_path="./test/result_solv_data")
