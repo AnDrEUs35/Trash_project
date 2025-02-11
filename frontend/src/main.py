02adc44e5fb735e50eaa0a5937d47394c9bdd13d
AstroBFU <48380696+AstroBFU@users.noreply.github.com>
2025-02-11 17:07:48 +0200

    Delete frontend directory

--- frontend/src/main.py
+++
@@ -1,9 +0,0 @@
-from . import validator
-
-def run_validator(data_path):
-    valid = validator.Validator(data_path)
-    valid.date_examination()
-    valid.start_time_examination()
-
-if __name__=='__main__':
-    run_validator('src/data.json') 
