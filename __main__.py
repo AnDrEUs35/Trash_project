ee5756f56e3c0265c716cb494d2063c1ab95814a
AstroBFU <48380696+AstroBFU@users.noreply.github.com>
2025-03-01 12:42:56 +0200

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,19 @@
+import frontend.src.main as frontend
+import astro_data.src.main as debris_data
+import solver.src.main as solver
+import handler.main as graph_handler
+import sys
+
+
+def main(config_path, output_path):
+    frontend.run_validator(config_path)
+    debris_data_path = debris_data.run(config_path)
+    solver.run(debris_data_path, output_path)
+
+    #arepo_output_path = './test'
+    #graph_handler.run(arepo_output_path, output_path)
+
+
+if __name__ == "__main__":
+    # main('./test/frontend_output.json', './test')
+    main(sys.argv[1], sys.argv[2])
